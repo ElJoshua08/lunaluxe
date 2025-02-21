1ca7a1e18bea94b8e04ec87f3cf8e3c696debf80
Joshua <74731460+ElJoshua08@users.noreply.github.com>
2025-02-21 11:49:51 +0100

    Update server.ts

--- src/lib/utils/supabase/server.ts
+++ src/lib/utils/supabase/server.ts
@@ -1,33 +1,38 @@
 import { createServerClient } from '@supabase/ssr';
 import { cookies } from 'next/headers';
 
-export async function createClient() {
-  const cookieStore = await cookies();
+const {
+  NEXT_PUBLIC_SUPABASE_URL: SUPABASE_URL,
+  NEXT_PUBLIC_SUPABASE_ANON_KEY: SUPABASE_KEY,
+} = process.env;
 
-  if(!cookieStore.getAll()) {
-    throw("No cookies stored");
+export async function createClient() {
+  if(!SUPABASE_URL || !SUPABASE_KEY) {
+    return { error: "No ENV variables found" }
   }
 
-  return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        getAll() {
-          return cookieStore.getAll();
-        },
-        setAll(cookiesToSet) {
-          try {
-            cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options)
-            );
-          } catch {
+  return {
+    supabase: createServerClient(
+      SUPABASE_URL,
+      SUPABASE_KEY,
+      {
+        cookies: {
+          getAll() {
+            return cookieStore.getAll();
+          },
+          setAll(cookiesToSet) {
+            try {
+              cookiesToSet.forEach(({ name, value, options }) =>
+                cookieStore.set(name, value, options)
+              );
+            } catch {
             // The `setAll` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing
             // user sessions.
-          }
+            }
+          },
         },
-      },
-    }
-  );
+      }
+    ); 
+  }
 }
