d7e0e6ffafd38aed1bb207ceeef88f1aba5b0810
Joshua <josuealejandrof926@gmail.com>
2025-04-19 04:48:08 +0200

    adding some comments on what should i do next.

--- src/app/(admin)/dashboard/products/create/page.tsx
+++ src/app/(admin)/dashboard/products/create/page.tsx
@@ -1,7 +1,14 @@
 "use client"
 
 import { Button } from "@/components/ui/button"
-import { Form, FormControl, FormField, FormItem } from "@/components/ui/form"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+} from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator"
 import { productBasicInfoSchema } from "@/lib/schema/auth"
@@ -49,33 +56,47 @@ export default function CreateProductPage() {
         {/* Here we change the form depending on the current step, maybe we can use a custom component for that */}
         <Form {...method}>
           <form className="flex h-full w-full flex-col items-start justify-start gap-y-4">
-            <FormField
-              control={method.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem className="w-full">
-                  <FormControl>
-                    <Input {...field} placeholder="Name" className="w-full" />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={method.control}
-              name="price"
-              render={({ field }) => (
-                <FormItem className="w-full">
-                  <FormControl>
-                    <Input
-                      {...field}
-                      placeholder="Ex. 100€"
-                      className="w-full"
-                      type="number"
-                    />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
+            <div className="flex h-full w-1/2 flex-col items-start justify-start gap-y-4">
+              <FormField
+                control={method.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem className="w-full">
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input {...field} placeholder="Name" className="w-full" />
+                    </FormControl>
+                    <FormDescription>
+                      The name you want to give to your product.
+                    </FormDescription>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={method.control}
+                name="price"
+                render={({ field }) => (
+                  <FormItem className="w-full">
+                    {/* Here in the price i need to make a custom price component to make it easier to use. */}
+                    <FormLabel>Price</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder="Ex. 100€"
+                        className="w-full"
+                        type="number"
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      The price you want to give to your product, please use
+                      commas for the decimal point.
+                    </FormDescription>
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            {/* Here a little chanllenge for myself, i want to make a custom component that is a rich text editor, having like bold, italic, underline, etc. I want to make use of it in the form for the product description. */}
           </form>
         </Form>
       </div>
